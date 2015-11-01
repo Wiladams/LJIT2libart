a123a4f50aa5387f36895395c25191c311643e8d
William Adams <spam@nanotechstyles.com>
2015-10-31 23:39:58 -0700

    cleanup so binding actually compiles

--- art_ffi.lua
+++ art_ffi.lua
@@ -1,13 +1,25 @@
+-- Reference
+--
+-- https://github.com/armon/libart
+--
+-- Despite its name, libart isn't actuall a library
+-- but rather a couple of .c/h files, which could be compiled
+-- into a library, or into an application direction.
+-- This binding works against the function interface, but you'll
+-- need to reference the library, or executable that actually contains
+-- the compiled code.
+--
+local ffi = require("ffi")
 
-
-#define NODE4   1
-#define NODE16  2
-#define NODE48  3
-#define NODE256 4
+ffi.cdef[[
+static const int NODE4   =1;
+static const int NODE16  =2;
+static const int NODE48  =3;
+static const int NODE256 =4;
+]]
 
 ffi.cdef[[
 static const int MAX_PREFIX_LEN = 10;
-
 
 
 typedef int(*art_callback)(void *data, const unsigned char *key, uint32_t key_len, void *value);
@@ -95,56 +107,24 @@ int art_tree_destroy(art_tree *t);
 ]]
 
 
-local function art_size(art_tree *t)
+local function art_size(t)
     return t.size;
 end
 
 ffi.cdef[[
-/**
- * Inserts a new value into the ART tree
- * @arg t The tree
- * @arg key The key
- * @arg key_len The length of the key
- * @arg value Opaque value.
- * @return NULL if the item was newly inserted, otherwise
- * the old value pointer is returned.
- */
+
 void* art_insert(art_tree *t, const unsigned char *key, int key_len, void *value);
 
-/**
- * Deletes a value from the ART tree
- * @arg t The tree
- * @arg key The key
- * @arg key_len The length of the key
- * @return NULL if the item was not found, otherwise
- * the value pointer is returned.
- */
 void* art_delete(art_tree *t, const unsigned char *key, int key_len);
 
-/**
- * Searches for a value in the ART tree
- * @arg t The tree
- * @arg key The key
- * @arg key_len The length of the key
- * @return NULL if the item was not found, otherwise
- * the value pointer is returned.
- */
 void* art_search(const art_tree *t, const unsigned char *key, int key_len);
 
-/**
- * Returns the minimum valued leaf
- * @return The minimum leaf or NULL
- */
 art_leaf* art_minimum(art_tree *t);
-
 
 art_leaf* art_maximum(art_tree *t);
 
-
 int art_iter(art_tree *t, art_callback cb, void *data);
-
 
 int art_iter_prefix(art_tree *t, const unsigned char *prefix, int prefix_len, art_callback cb, void *data);
 ]]
 
-local Lib_art = ffi.load("check")
